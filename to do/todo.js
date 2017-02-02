91aad1fe183e435877ecaf4c48d26c697cdb5ed0
Glitch773 <j-le@kellogg.northwestern.edu>
2017-02-02 01:14:26 -0600

    To do added

---
+++ to do/todo.js
@@ -0,0 +1,27 @@
+$("c1").ready(function(){
+    $(".submit1").click(function(){
+      $(this).hide();
+      $("#c1").hide();
+    })
+  })
+
+$("c2").ready(function(){
+    $(".submit2").click(function(){
+      $(this).hide();
+      $("#c2").hide();
+    })
+  })
+
+$("c3").ready(function(){
+    $(".submit3").click(function(){
+      $(this).hide();
+      $("#c3").hide();
+    })
+  })
+
+$("c4").ready(function(){
+    $(".submit4").click(function(){
+      $(this).hide();
+      $("#c4").hide();
+    })
+})
